e8d6612fd0a46e89370d0d8b71df1ea85ab25cc3
Oskar Kalbarczyk <oskalbarczyk@gmail.com>
2025-06-24 18:59:18 +0200

    generate components

---
+++ src/app/components/element-table/element-table.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-element-table',
+  imports: [],
+  templateUrl: './element-table.html',
+  styleUrl: './element-table.scss'
+})
+export class ElementTable {
+
+}
